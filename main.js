5c434348700703c929de6877871fcf030dbac7c6
doileo <leovchind@gmail.com>
2023-09-17 18:50:04 +0300

    Created a step by step plan before writing the code

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 "use strict";
+/* 
+1. Make a selection of which pledge to make:
+- Add event listeners to the "Select reward" buttons in your pledge options.
+- When a button is clicked, open the selection modal with the corresponding pledge details.
+
+2. See an updated progress bar and total money raised based on their pledge total after confirming a pledge:
+- Create variables to track the total money raised and the number of backers.
+- Update these variables when a pledge is confirmed.
+- Update the progress bar based on the total money raised.
+
+3. See the number of total backers increment by one after confirming a pledge:
+- Increment the number of backers when a pledge is confirmed.
+- Toggle whether or not the product is bookmarked:
+
+4. Add an event listener to the "Bookmark" button.
+- Toggle the bookmark state when the button is clicked.
+
+5. Make the hamburger menu work:
+- Add an event listener for opening the mobile menu
+- Add an event listener for closing the mobile menu
+- Add an Event listener for clicking outside the mobile menu to close it
+*/
