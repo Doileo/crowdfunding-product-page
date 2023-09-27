c89cd82c2434f7145545aa7b9a1a5f2d2546417b
doileo <leovchind@gmail.com>
2023-09-27 12:44:56 +0300

    Refactor mobile menu functionality for toggling open and close states

--- main.js
+++ main.js
@@ -8,43 +8,38 @@ const closeMenuIcon = document.querySelector('.close-menu-icon');
 const modalBg = document.getElementById('modal-bg');
 const logo = document.querySelector('.logo')
 
-// Function to open the mobile menu
-function openMobileMenu() {
-    mobileMenu.style.display = 'block';
-    modalBg.style.display = 'block';
-
-    // Toggle the display property between open and close icons
-    openMenuIcon.style.display = 'none';
-    closeMenuIcon.style.display = 'block';
-
-    logo.style.zIndex = '1000';
-    closeMenuIcon.style.zIndex = '1000';
-}
-
-// Function to close the mobile menu
-function closeMobileMenu() {
-    mobileMenu.style.display = 'none';
-    modalBg.style.display = 'none';
-
-    // Toggle the display property between open and close icons
-    openMenuIcon.style.display = 'block';
-    closeMenuIcon.style.display = 'none';
-
-    // Reset the logo and close icon appearance
-    logo.style.zIndex = 'auto';
-    hamburgerIcon.style.display = 'block';
+// Function to toggle the mobile menu
+function toggleMobileMenu() {
+    if (mobileMenu.style.display === 'none' || mobileMenu.style.display === '') {
+        // Mobile menu is currently closed, so here it is to open it
+        mobileMenu.style.display = 'block';
+        modalBg.style.display = 'block';
+        openMenuIcon.style.display = 'none';
+        closeMenuIcon.style.display = 'block';
+        logo.style.zIndex = '1000';
+        closeMenuIcon.style.zIndex = '1000';
+    } else {
+        // Mobile menu is currently open, so we need to close it
+        mobileMenu.style.display = 'none';
+        modalBg.style.display = 'none';
+        openMenuIcon.style.display = 'block';
+        closeMenuIcon.style.display = 'none';
+        logo.style.zIndex = 'auto';
+        hamburgerIcon.style.display = 'block';
+    }
 }
 
 // Initially show the hamburger icon and hide the close icon
 closeMenuIcon.style.display = 'none';
 
-// Event listener for clicking the hamburger icon
-hamburgerIcon.addEventListener('click', openMobileMenu);
-
-// Event listener for clicking the close icon
-closeMenuIcon.addEventListener('click', closeMobileMenu);
+// Event listener for clicking the hamburger icon or close icon to toggle the mobile menu
+hamburgerIcon.addEventListener('click', toggleMobileMenu);
+closeMenuIcon.addEventListener('click', toggleMobileMenu);
 
 // Event listener for clicking outside the mobile menu to close it
-modalBg.addEventListener('click', closeMobileMenu);
-
-// Use the if else statement here
+modalBg.addEventListener('click', function (event) {
+    if (event.target === modalBg) {
+        // Only close the menu if the click occurred on the modal background itself
+        toggleMobileMenu();
+    }
+});
